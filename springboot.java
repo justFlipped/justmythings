e59d65df935a7ef2f4c53015110387c76bd68657
Flipped <734776772@qq.com>
2020-07-31 15:39:15 +0800

    modifiy in sptingboot.java

---
+++ springboot.java
@@ -0,0 +1,14 @@
+public class springboot{
+
+      public static void main(String[] args){
+                   System.out.println("!!!!!!!!!!!!!!!!!!!!!!!");
+                   String hello=helloGit();
+		   System.out.println(hello);
+      }
+      public static String  helloGit(){
+
+                    String str="hello git and github";
+		    return str;
+      }
+   
+}
